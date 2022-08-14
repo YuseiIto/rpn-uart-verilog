c826cb91a7640c2c6968a30727fe980fcb7a22ef
Yusei Ito <me@yuseiito.com>
2022-08-15 00:05:08 +0900

    Modify: add print_ready output to op_controller

--- op_controller/op_controller.v
+++ op_controller/op_controller.v
@@ -1,10 +1,14 @@
-module op_controller(clk,is_op,arg_cnt,pop_cnt,ans_ready);
+module op_controller(clk,is_op,op,arg_cnt,pop_cnt,ans_ready,print_ready);
 	input clk;
 	input is_op;
-	input [1:0] arg_cnt;
-	
+	input [3:0] op;
+	input [1:0] arg_cnt;	
+
 	output [1:0] pop_cnt;
 	output reg ans_ready;
+	output print_ready;
+
+	parameter OP_ADD = 4'd0, OP_SUB = 4'd1, OP_MUL = 4'd2, OP_DIV = 4'd3, OP_POP = 4'd4, OP_UNKKOWN = 4'hf;
 
 	reg prev_is_op;
 	initial begin
@@ -22,4 +26,6 @@ module op_controller(clk,is_op,arg_cnt,pop_cnt,ans_ready);
 	end
 
 	assign pop_cnt=ans_ready?arg_cnt:0;	
+	assign print_ready= (op==OP_POP) & ans_ready;
+
 endmodule
--- op_controller/op_controller_test.v
+++ op_controller/op_controller_test.v
@@ -10,23 +10,31 @@ always #(CLK_PERIOD/2) clk = ~clk;
 
 /* Testbench body */
 parameter TIMESCALE_PER_BIT = 8681;
+parameter OP_ADD = 4'd0, OP_SUB = 4'd1, OP_MUL = 4'd2, OP_DIV = 4'd3, OP_POP = 4'd4, OP_UNKKOWN = 4'hf;
 
 reg is_op;
 reg [1:0] arg_cnt;
+reg [3:0] op;
 wire [1:0] pop_cnt;
 wire ans_ready;
+wire print_ready;
 
-op_controller op_ctrl_inst(clk,is_op,arg_cnt,pop_cnt,ans_ready);
-
+op_controller op_ctrl_inst(clk,is_op,op,arg_cnt,pop_cnt,ans_ready,print_ready);
 
 initial begin
-	clk=0;
-	is_op=0;
-	arg_cnt = 2'd2;
+		clk=0;
+		is_op=0;
+		op=OP_ADD;
+		arg_cnt = 2'd2;
 	#(TIMESCALE_PER_BIT) 
 		is_op = 1;
 	#(TIMESCALE_PER_BIT) 
 		is_op = 0;
+	#(TIMESCALE_PER_BIT)
+		op = OP_POP;
+		is_op=1;
+	#(TIMESCALE_PER_BIT)
+		is_op=0;
 	#10 $finish;
 end
 
