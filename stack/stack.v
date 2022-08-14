d6a958f13c1eaa89fd9c11e5a21f89c551b4b4a3
Yusei Ito <me@yuseiito.com>
2022-08-15 03:21:40 +0900

    Bugfix: fix stack bugs

--- stack/stack.v
+++ stack/stack.v
@@ -11,23 +11,19 @@ reg [15:0] content[0:STACK_DEPTH-1];
 
 reg [STACK_ADDR_WIDTH-1:0] sp;
 assign first=content[sp-1];
-assign second=content[sp-2];
+assign second=sp>1 ? content[sp-2] : 0;
 
 initial begin
-	sp <= 1;
+	sp <= 0;
 end
 
 always@(posedge clk) begin
 
-	if (sp>pop_cnt) begin
-		if(wen) 
-		begin
-			if (sp>pop_cnt&&sp<STACK_DEPTH) begin
-					content[sp-pop_cnt] <= din;
+	if (sp>=pop_cnt) begin
+		if(wen && sp<STACK_DEPTH) begin
+				content[sp-pop_cnt] <= din;
 				sp<=sp-pop_cnt+1;
-			end
-		end
-		else sp<=sp-pop_cnt;
+		end else sp<=sp-pop_cnt;
 	end
 end
 
