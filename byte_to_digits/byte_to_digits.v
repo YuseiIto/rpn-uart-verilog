4d1b60d4da34e6db960a5dcc6e69e9c13545fef7
Yusei Ito <me@yuseiito.com>
2022-08-15 04:03:38 +0900

    Add: Byte to digits

---
+++ byte_to_digits/byte_to_digits.v
@@ -0,0 +1,49 @@
+module byte_to_digits(clk,din,wen,dout,sending);
+input clk;
+input [15:0] din;
+input wen;
+output reg [3:0] dout;
+output reg sending;
+
+reg [15:0] tmp;
+reg [2:0] power;
+reg finish;
+
+initial begin
+	sending = 0;
+	dout = 0;
+	tmp = 0;
+	power = 0;
+	finish = 0;
+end
+
+always@(posedge clk) begin
+	if (wen) begin
+		tmp<=din%10**5;
+		dout<=din/10**5;
+		/* Since the number is 16 bit length,
+		 & the first digit (din/10**5) is always zero and looks unnessessary at frist.
+		 However, to make sure that the "sending" signal is not stuck at 1,
+		 send a "definitely zero" msb */
+		sending<=1;
+		power<=4;
+	end
+
+	if (power>0) begin
+		dout<=tmp/10**power;
+		power<=power-1;
+		tmp<= tmp%10**power;
+	end
+	else if (power==0 && sending)begin
+		dout<=tmp;
+		tmp<=0;
+		finish <=1;
+	end
+
+	if (finish) begin
+		sending<=0;
+		finish<=0;
+	end
+end
+
+endmodule
---
+++ byte_to_digits/byte_to_digits_test.v
@@ -0,0 +1,41 @@
+
+`timescale 1ns/1ps
+
+module byte_to_digits_testbench();
+
+/* Test bench common setup */
+parameter CLK_PERIOD = 8; // Clock period in timescale(1ns in this case)
+reg clk;
+always #(CLK_PERIOD/2) clk = ~clk;
+
+
+/* Testbench body */
+
+reg [15:0] din;
+reg wen;
+
+wire [3:0] dout;
+wire sending;
+
+byte_to_digits encoder (clk,din,wen,dout,sending);
+
+initial begin
+	clk=0;
+	din=16'd123;
+	wen=0;
+	#10 wen=1;
+	#8 wen=0;
+	#50 wen=1;
+		  din=16'd65535;
+	#8 wen=0;
+	#50 $finish;
+end
+
+
+/* Icarus Verilog simulation */
+initial begin
+	$dumpfile("byte_to_digits.vcd");
+	$dumpvars(0, encoder);
+end
+
+endmodule
