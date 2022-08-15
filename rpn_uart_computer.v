e787ca6e4a1924c2babc896d5f1e31e778dcb2f8
Yusei Ito <me@yuseiito.com>
2022-08-15 15:57:01 +0900

    Add: top module(wip)

---
+++ rpn_uart_computer.v
@@ -0,0 +1,17 @@
+module rpn_uart_computer(clk,rx_in);
+	input clk;
+	input rx_in;
+
+	wire op_ready;
+	wire [3:0] op;
+	wire num_ready;
+	wire [15:0] num;
+
+	iterpreter interpreter_inst(clk,rx_in,op_ready,op,num_ready,num);
+
+	wire [15:0] res_value;
+	wire res_ready;
+	rpn rpn_inst(clk,op_ready,op,num_ready,num,res_value,res_ready);
+
+	
+endmodule
---
+++ rpn_uart_computer_test.v
@@ -0,0 +1,49 @@
+`timescale 1ns/1ps
+
+module rpn_uart_computer_testbench();
+
+/* Test bench common setup */
+parameter CLK_PERIOD = 8; // Clock period in timescale(1ns in this case)
+reg clk;
+always #(CLK_PERIOD/2) clk = ~clk;
+
+
+/* Testbench body */
+parameter TIMESCALE_PER_BIT = 8681;
+
+
+parameter INPUT_STRING="12 12 + 3 *\n";
+parameter INPUT_STRING_LENGTH = 12;
+
+integer c,b; // Char/Bit index
+
+reg rx_in;
+
+wire op_ready,num_ready;
+wire [15:0] num;
+wire [3:0] op;
+
+rpn_uart_computer main_module(clk,rx_in);
+
+initial begin
+	clk=0;
+	rx_in=1;
+	for (c=0;c<INPUT_STRING_LENGTH;c++) begin
+		#(TIMESCALE_PER_BIT) rx_in = 0; // Start bit
+		#8 
+		for (b=0;b<8;b++) begin
+			#(TIMESCALE_PER_BIT) rx_in = INPUT_STRING[(INPUT_STRING_LENGTH-1-c)*8+b];
+		end
+		#(TIMESCALE_PER_BIT) rx_in = 1; //Stop bit
+	end
+	#(TIMESCALE_PER_BIT*INPUT_STRING_LENGTH*10) $finish;
+end
+
+
+/* Icarus Verilog simulation */
+initial begin
+	$dumpfile("rpn_uart_computer.vcd");
+	$dumpvars(0, rpn_uart_computer_testbench);
+end
+
+endmodule
