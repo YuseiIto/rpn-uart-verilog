e1ba1d3f046c219715eecd117fca8af2ec72f090
Yusei Ito <me@yuseiito.com>
2022-08-16 23:22:46 +0900

    Modify: change some triggers to avoid multi driven net

--- uart_rx/uart_rx.v
+++ uart_rx/uart_rx.v
@@ -1,5 +1,5 @@
 module uart_rx(clk,rx_in,out,ready);
-/* clk has to be 125MHz to generate the correct baudrate */
+/* clk has to be 25MHz to generate the correct baudrate */
 
 input clk,rx_in;
 output [7:0] out;
@@ -11,18 +11,15 @@ reg baudrate_clk_en; // If runnning baudrate clk
 wire baudrate_clk;
 baudrate_clk_gen clk_gen_inst (clk,baudrate_clk_en,baudrate_clk);
 
+reg prev_rx_in;
+reg prev_baudrate_clk;
+
 initial begin
 	baudrate_clk_en = 0;
 	ready = 0;
+	prev_rx_in=0;
+	prev_baudrate_clk<=0;
 end
-
-always @(negedge rx_in) begin
-	if (baudrate_clk_en==0) begin
-		baudrate_clk_en<=1;
-		rx_data<= 10'b1111111111; // Fill with 1 to detect start bit
-	end
-end
-
 
 always@(posedge clk) begin
 	if(baudrate_clk_en) begin
@@ -31,24 +28,27 @@ always@(posedge clk) begin
 			// Byte is ready
 			ready <= 1;
 			baudrate_clk_en<=0;
+		end else if(~prev_baudrate_clk&&baudrate_clk) begin
+			 rx_data[0] <= rx_in;
+			 rx_data[1] <= rx_data[0];
+			 rx_data[2] <= rx_data[1];
+			 rx_data[3] <= rx_data[2];
+			 rx_data[4] <= rx_data[3];
+			 rx_data[5] <= rx_data[4];
+			 rx_data[6] <= rx_data[5];
+			 rx_data[7] <= rx_data[6];
+			 rx_data[8] <= rx_data[7];
+			 rx_data[9] <= rx_data[8];
 		end
+
+	end else if (prev_rx_in&&~rx_in && baudrate_clk_en==0) begin // negedge rx_in
+		baudrate_clk_en<=1;
+		rx_data <= 10'b1111111111;
 	end
-
+  
 	if (ready) ready<=0; // Ready is high only for one clock cycle
-
-end
-
-always@(posedge baudrate_clk) begin 
- rx_data[0] <= rx_in;
- rx_data[1] <= rx_data[0];
- rx_data[2] <= rx_data[1];
- rx_data[3] <= rx_data[2];
- rx_data[4] <= rx_data[3];
- rx_data[5] <= rx_data[4];
- rx_data[6] <= rx_data[5];
- rx_data[7] <= rx_data[6];
- rx_data[8] <= rx_data[7];
- rx_data[9] <= rx_data[8];
+	prev_rx_in<=rx_in;
+	prev_baudrate_clk<=baudrate_clk;
 end
 
 // Remove stop/start bit. Pay attention to the bit order
--- uart_tx/uart_tx.v
+++ uart_tx/uart_tx.v
@@ -15,11 +15,15 @@ baudrate_clk_gen clk_gen_inst (clk,baudrate_clk_en,baudrate_clk);
 reg [3:0] send_index; // Count up to 9 (1 startbit + 8 data bits + 1 stop bit)
 wire [9:0] send_data = {1'b1,tx_data,1'b0}; // Start/stop bit.
 
+reg prev_baudrate_clk;
+
 initial begin
 	ready=1;
 	tx_out=1;
 	send_index=0;
+	prev_baudrate_clk=0;
 end
+
 
 always@(posedge clk) begin
 	if (ready && wen) begin
@@ -28,22 +32,20 @@ always@(posedge clk) begin
 
 			// Start transmission
 			baudrate_clk_en<=1;
+		end else if (send_index==11) begin
+			baudrate_clk_en<=0;
 			send_index<=0;
-	end
-end
+		end else if (~prev_baudrate_clk&&baudrate_clk) begin // posedge baudrate_clk
+		tx_out<=send_data[send_index];
 
-
-always@(posedge baudrate_clk) begin
-	tx_out<=send_data[send_index];
-	if(send_index==10) begin
-		send_index<=0;
-		baudrate_clk_en<=0;
-		ready<=1;
-		tx_out<=1;
+		if(send_index==10) begin
+			ready<=1;
+			tx_out<=1;	
 		end
-	else 	send_index<=send_index+1;
+				send_index<=send_index+1;
+	end
 
+	prev_baudrate_clk<=baudrate_clk;
 end
-
 
 endmodule
